73e2c19641fb525c2c850cc4f1d01f887ed0fbef
Mickael SCHIMPF <mischimpf@Helfrich.local>
2018-11-15 09:52:03 +0100

    creating pagination for playing episodes

--- view/home.php
+++ view/home.php
@@ -25,30 +25,52 @@
   <h2 class="titleSection">Les épisodes</h2>
 <div style="padding-top: 60px" class="container">
   <div class="col-md-12">
-    <p class="title_episode">Nom de l'épisode</p>
-    <p class="informations_episode">Date de publication :</p>
-    <p class="informations_episode">Date de modification :</p>
 
-    <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros,
-      armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis
-      Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velu
-      t mandato principis iurandi crebritate confirmans et statim inquit exsurge et
-      inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi
-      quondam peremptum Constantini filium accepimus Crispum.</p>
-    </div>
-    <div style="margin: 40px 0px 40px 0px" class="col-md-12">
-      <p class="title_episode">Nom de l'épisode</p>
-      <p class="informations_episode">Date de publication :</p>
-      <p class="informations_episode">Date de modification :</p>
+    <table id="myTable">
+      <thead>
+        <tr>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><p class="title_episode">Nom de l'épisode</p></td>
+        </tr>
+        <tr>
+          <td><p class="informations_episode">Date de publication :</p></td>
+        </tr>
+        <tr>
+          <td><p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros,
+            armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis
+            Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velu
+            t mandato principis iurandi crebritate confirmans et statim inquit exsurge et
+            inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi
+            quondam peremptum Constantini filium accepimus Crispum.</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-      <p>Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros,
-        armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis
-        Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velu
-        t mandato principis iurandi crebritate confirmans et statim inquit exsurge et
-        inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi
-        quondam peremptum Constantini filium accepimus Crispum.</p>
-      </div>
+
+
+
+
+
     </div>
+
+    </div>
+
+    <script>
+    $(document).ready( function () {
+  $('#myTable').DataTable({
+    "pagingType": "simple",
+    "bLengthChange": false,
+    "searching": false,
+    "bInfo" : false,
+    "ordering" : false
+  });
+} );
+    </script>
 
     <?php $content = ob_get_clean(); ?>
 
--- view/template.php
+++ view/template.php
@@ -13,6 +13,9 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+
+
 
   <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
@@ -34,6 +37,7 @@
       <script href="../public/js/jquery-3.3.1.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <script src="../public/js/jquery.dataTables.min.js"></script>
     </body>
 
     </html>
